09c0cb318ea63659c751d7ed579ae092331cbf52
Igors78 <oleinikovsigors@gmail.com>
2021-08-05 12:33:28 +0300

    Header correct

--- philo.h
+++ philo.h
@@ -6,7 +6,16 @@
 /*   By: ioleinik <ioleinik@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 15:59:00 by ioleinik          #+#    #+#             */
-/*   Updated: 2021/08/01 15:59:07 by ioleinik         ###   ########.fr       */
+/*   Updated: 2021/08/05 12:32:53 by ioleinik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+
+#endif
